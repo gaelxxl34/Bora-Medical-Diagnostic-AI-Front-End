bd3a258de0c35a7796f2e1c026484502b7e80c66
esdraswite <witeesdras@gmail.com>
2025-05-01 07:35:57 +0300

    feat:ajouter parallel route child

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -44,6 +46,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {modal}
       </body>
     </html>
   );
