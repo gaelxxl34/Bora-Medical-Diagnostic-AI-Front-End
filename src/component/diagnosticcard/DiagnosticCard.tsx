5d396a068982d26d6ce279b5da1edaaa0728c0b0
esdraswite <witeesdras@gmail.com>
2025-05-02 18:32:49 +0300

    test database functions calls

--- src/component/diagnosticcard/DiagnosticCard.tsx
+++ src/component/diagnosticcard/DiagnosticCard.tsx
@@ -1,7 +1,6 @@
 import diagnosticImages from "@/data/diagnosticImages";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const DiagnosticCard = () => {
   return (
